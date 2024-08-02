32bc944ccb14867996b1dd1accdad6e357c4b4dc
Nikita Stephan <dev@mail.atheistnapkin.de>
2024-08-02 03:18:50 +0200

    add map test

--- slicetransform_test.go
+++ slicetransform_test.go
@@ -4,6 +4,7 @@ import(
 	"testing"
 	"github.com/niksteph/slicetransform"
 	"slices"
+	"fmt"
 )
 
 func TestFilter(t *testing.T) {
@@ -11,6 +12,17 @@ func TestFilter(t *testing.T) {
 	want := []int{2,4,8}
 	got := slicetransform.Filter(test, func(n int) bool {
 		return n%2 == 0
+	})
+	if !slices.Equal(want,got) {
+		t.Errorf("want:%v, but got %v", want, got)
+	}
+}
+
+func TestMap(t *testing.T) {
+	test := []int{2,3,4,7,8}
+	want := []string{"10","11","100","111","1000"}
+	got := slicetransform.Map(test, func(n int) string {
+		return fmt.Sprintf("%b", n)
 	})
 	if !slices.Equal(want,got) {
 		t.Errorf("want:%v, but got %v", want, got)
