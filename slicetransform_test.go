42aea69cae7e6f65b7d4e30498ee3c665a348bd7
Nikita Stephan <dev@mail.atheistnapkin.de>
2024-08-02 23:17:50 +0200

    format code

--- slicetransform_test.go
+++ slicetransform_test.go
@@ -1,39 +1,39 @@
 package slicetransform_test
 
-import(
-	"testing"
+import (
+	"fmt"
 	"github.com/niksteph/slicetransform"
 	"slices"
-	"fmt"
+	"testing"
 )
 
 func TestFilter(t *testing.T) {
-	test := []int{2,3,4,7,8}
-	want := []int{2,4,8}
+	test := []int{2, 3, 4, 7, 8}
+	want := []int{2, 4, 8}
 	got := slicetransform.Filter(test, func(n int) bool {
 		return n%2 == 0
 	})
-	if !slices.Equal(want,got) {
+	if !slices.Equal(want, got) {
 		t.Errorf("want:%v, but got %v", want, got)
 	}
 }
 
 func TestMap(t *testing.T) {
-	test := []int{2,3,4,7,8}
-	want := []string{"10","11","100","111","1000"}
+	test := []int{2, 3, 4, 7, 8}
+	want := []string{"10", "11", "100", "111", "1000"}
 	got := slicetransform.Map(test, func(n int) string {
 		return fmt.Sprintf("%b", n)
 	})
-	if !slices.Equal(want,got) {
+	if !slices.Equal(want, got) {
 		t.Errorf("want:%v, but got %v", want, got)
 	}
 }
 
 func TestReduce(t *testing.T) {
-	test := []int{2,3,4,7,8}
-	want := 2+3+4+7+8
+	test := []int{2, 3, 4, 7, 8}
+	want := 2 + 3 + 4 + 7 + 8
 	got := slicetransform.Reduce(test, 0, func(intermed, current int) int {
-		return intermed+current
+		return intermed + current
 	})
 	if want != got {
 		t.Errorf("want:%v, but got %v", want, got)
